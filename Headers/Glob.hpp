f59d703e6953a29c47e139ebf0f55952d6f93bfa
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-04-26 20:15:17 +0200

    vUpdate

--- Headers/Glob.hpp
+++ Headers/Glob.hpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 23.04.25
+// Version: 23.04.26
 // EndLic
 #pragma once
 
